08e14d87dcae15c50fb615eebfa3555ba01e94c9
A-peli <aapeli.huhtinen@student.laurea.fi>
2025-05-18 18:43:48 +0300

    fixed final bugs.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -78,7 +78,7 @@ try {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-} catch (error) {
+} catch {
   return {
     message: 'Database Error: Failed to Create Invoice.',
   };
@@ -117,7 +117,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
+  } catch {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
